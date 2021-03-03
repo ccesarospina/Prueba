15d7cb82a951663c176b0d9625a848cbc7271689
ASUS <ASUS@HBOUQUETERA01>
2021-03-02 23:08:40 -0500

    Primer cambio

---
+++ src/pruebagit/PruebaGit.java
@@ -0,0 +1,12 @@
+
+package pruebagit;
+
+
+public class PruebaGit {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola, esta es la primera prubea con GIT");
+    }
+    
+}
